b4688691ff52f8f5dba5f20a7aaa7856ee7ee5e7
rishabhluitel <47209302+rishabhluitel@users.noreply.github.com>
2025-02-01 10:03:35 +0000

    learnt variable

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144553;
+let accountEmail="rishabh@gmail.com";
+var accountPassword="12345";
+accountCity="Nepal"; //dont use this ..
+let accountState;
+
+//let--local scope
+//var--global scope not used because of issue in functional scope and block scope
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
