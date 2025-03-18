8b87ee25dd4cbcf4b9afda357c463d4f0c555d61
Rishabh Luitel <47209302+rishabhluitel@users.noreply.github.com>
2025-03-18 14:19:07 +0000

    message

--- 09_advance/promise.js
+++ 09_advance/promise.js
@@ -89,14 +89,14 @@
 //   }
 // }
 // getAllUsers();
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response)=>{
-return response.json()
-})
-.then((data)=>{
-console.log(data);
 
-})
-.catch((error)=>{
-  console.log(error)
-})
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
